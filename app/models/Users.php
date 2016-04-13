40470e21367c799aa5d34a47d92ecbdb166dd1ee
Punit Dhoot <punitdhoot1@gmail.com>
2016-04-14 00:56:13 +0530

    Started writing Users

--- app/models/Users.php
+++ app/models/Users.php
@@ -18,4 +18,35 @@ class Users
 	{
 	    return true;
 	}
+
+	public static function get_db()
+	{
+		include "../config/config.php";
+		return new \PDO("mysql:dbname=".$configs['db_name'].";host=".$configs['host'] ,  $configs['username'] , $configs['password']);
+	}
+
+	public static function check_login($username, $password)
+	{
+		$db = self::get_db();
+
+		$passhash = md5($password);
+
+		$statement = $db->prepare("SELECT * FROM USER_DB WHERE username = :username AND password = :password");
+		$statement->bindValue(":username" , $username);
+		$statement->bindValue(":password" , $passhash);
+
+		$statement->execute();
+
+		$row = $statement->fetch(\PDO::FETCH_ASSOC);
+		if($row)
+		{
+			return true;
+		}	
+		else
+		{
+			return false;
+		}
+
+	}
+
 }
